83c9edcd6093f250e686d71b5a233d4bc74619f1
thermobee <thermobeelearns@gmail.com>
2016-07-13 22:55:09 +0000

    Refactor main logic to avoid hardcoding the languages

--- language_detector/main.py
+++ language_detector/main.py
@@ -6,23 +6,16 @@ from .languages import LANGUAGES
 
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
-    input_text = []
-    input_text = text.split(' ') 
 
-    spanish_counter = 0 
-    german_counter = 0
-    english_counter = 0
+    input_text = text.split() 
+
+    counter_dict = {'spanish': 0, 'german': 0, 'english': 0}
+
+    for language in languages:
+        counter = 0
+        for word in language['common_words']:
+            if word in input_text:
+                counter += 1
+            counter_dict[language['name']]= counter
     
-    for word in languages[0]['common_words']:
-        if word in input_text:
-            spanish_counter += 1
-    for word in languages[1]['common_words']:
-        if word in input_text:
-            german_counter += 1
-    for word in languages[-1]['common_words']:
-        if word in input_text:
-            english_counter += 1
-            
-    total_counts = {'spanish': spanish_counter, 'german': german_counter, 'english': english_counter }
-    
-    return max(total_counts, key = total_counts.get)
+    return max(counter_dict, key = counter_dict.get)
